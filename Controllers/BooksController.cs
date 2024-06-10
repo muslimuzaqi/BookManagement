c42a680d3eedd78c3f809833aa88f8082fd712aa
musli muzaqi <musli.muzaqi@hotmail.com>
2024-06-10 12:13:46 +0200

    Added BooksController

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using BookMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var books = context.Books.ToList();
+            return View(books);
+        }
+    }
+}
