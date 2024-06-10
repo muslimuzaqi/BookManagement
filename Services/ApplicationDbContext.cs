cde15858cfdd78c95b382674859f494c9bf205c2
musli muzaqi <musli.muzaqi@hotmail.com>
2024-06-10 11:27:10 +0200

    Configured appsettings.json and Program.cs and added Migrations

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookMVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        public DbSet<Book> Books { get; set; }   
+    }
+}
